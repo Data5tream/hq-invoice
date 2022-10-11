d13ed319c843c62391dbb854fcb4b7faa78289da
Data5tream <6423657+Data5tream@users.noreply.github.com>
2022-10-11 22:53:42 +0200

    chore: setup vite & balmUI

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <UiTopAppBar contentSelector="main">
+    HQ Invoice
+  </UiTopAppBar>
+
+  <main>
+    Content> {{ test }}
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { UiTopAppBar } from "balm-ui";
+
+const test = ref('test1');
+</script>
+
+<style scoped>
+
+</style>
