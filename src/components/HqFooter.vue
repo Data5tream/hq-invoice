5a1a1b4f9b6ca9054c192d3186241eda6c6e6835
Data5tream <6423657+Data5tream@users.noreply.github.com>
2022-10-16 22:21:22 +0200

    chore: add license file and improve footer

--- src/components/HqFooter.vue
+++ src/components/HqFooter.vue
@@ -1,6 +1,11 @@
 <template>
   <footer>
-    &copy; Simon Barth, 2022
+    <div>
+      <span>&copy; Simon Barth, 2022</span>
+      <a href="https://github.com/Data5tream/hq-invoice" target="_blank" rel="nofollow">Github</a>
+      <span>Licensed under GNU AGPLv3</span>
+    </div>
+    <span>v0.1.0</span>
   </footer>
 </template>
 
@@ -12,9 +17,24 @@
 footer {
   display: flex;
   align-items: center;
-  color: #fff;
+  justify-content: space-between;
+  color: var(--mdc-theme-on-primary);
   background: var(--mdc-theme-primary);
   height: 64px;
   padding: 16px 32px;
+}
+footer > div > * {
+  padding: 0 8px;
+  border-right: 1px solid var(--mdc-theme-on-primary);
+}
+footer > div > *:first-child {
+  padding-left: 0;
+}
+footer > div > *:last-child {
+  border-right: 0;
+}
+
+a,a:visited {
+  color: var(--mdc-theme-on-primary);
 }
 </style>
