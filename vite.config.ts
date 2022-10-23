461bb621aa39beaae0db9339c2102ff8a986c0b0
Data5tream <6423657+Data5tream@users.noreply.github.com>
2022-10-23 22:13:31 +0200

    chore: correct tsconfig settings for main.ts import

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
+      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.js',
       'balm-ui-css': 'balm-ui/dist/balm-ui.css'
     }
   }
