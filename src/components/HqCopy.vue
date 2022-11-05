9acf71f3e2c09c065cf84db977a441370fcd5d49
Data5tream <6423657+Data5tream@users.noreply.github.com>
2022-11-05 23:14:30 +0100

    feat: show notification on copy

--- src/components/HqCopy.vue
+++ src/components/HqCopy.vue
@@ -2,9 +2,16 @@
   <div v-copy="copy(val)">
     <slot />
   </div>
+  <Teleport to="body">
+    <div v-if="showNotification" class="notification" :class="{ visible }">
+      Daten kopiert
+    </div>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineProps({
   val: {
     type: String,
@@ -12,10 +19,28 @@ defineProps({
   },
 });
 
+const showNotification = ref(false);
+const visible = ref(false);
+
+const showNotif = () => {
+  showNotification.value = true;
+  setTimeout(() => {
+    visible.value = true;
+    setTimeout(hideNotif, 2000);
+  }, 100);
+};
+
+const hideNotif = () => {
+  visible.value = false;
+  setTimeout(() => {
+    showNotification.value = false;
+  }, 1000);
+};
+
 const copy = (text: string) => ({
   text,
   success: () => {
-    console.log('copy');
+    showNotif();
   },
 });
 </script>
@@ -24,4 +49,21 @@ const copy = (text: string) => ({
 div {
   cursor: pointer;
 }
+
+.notification {
+  position: fixed;
+  bottom: 80px;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #2c3e50;
+  color: #fff;
+  padding: 8px 32px;
+
+  opacity: 0;
+  transition: opacity 1s ease;
+}
+.notification.visible {
+  opacity: 1;
+}
+
 </style>
